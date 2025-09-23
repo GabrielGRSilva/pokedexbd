0c8e4f9246e670064c163d6b7cb5cdd97bb6926a
Gabriel G. R. Silva <gabrielgrsilva@outlook.com.br>
2025-09-23 09:48:42 -0300

    Add input/output logic in main.ts using Node

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,41 @@
-function main() {
-  console.log("Hello, world!");
+// repl.js actually refers to repl.ts
+import { startREPL, cleanInput } from "./repl.js";
+
+import { createInterface } from 'node:readline';
+import { stdin, stdout } from 'node:process';
+
+function callback(input: string) {
+  if (input.length === 0) {
+    rl.prompt();
+    return;
+  } else {
+  const cleanedInput = cleanInput(input); 
+  console.log(`Your command was: <${cleanedInput}>`);
+  rl.prompt();
+  return;
+  }
 }
 
-main(); 
+const rl = createInterface({ //Standard interface which waits user input and answers
+  input: stdin,
+  output: stdout,
+  prompt: "Pokedex >",
+});
+
+function main() {
+  startREPL();
+  rl.prompt();
+  rl.on("line", (input) => {
+    if (input.length === 0) {
+    rl.prompt();
+    return;
+  } else {
+  const cleanedInput = cleanInput(input); 
+  console.log(`Your command was: ${cleanedInput[0]}`);
+  rl.prompt();
+  return;
+  }});
+  }
+
+
+main();
--- src/repl.ts
+++ src/repl.ts
@@ -1,4 +1,5 @@
+export function startREPL() {}
+
 export function cleanInput(input: string): string[] {
     return input.toLowerCase().trim().split(" "); //User input is "case lowered", trimmed from whitespaces and split for each word
 }
-
